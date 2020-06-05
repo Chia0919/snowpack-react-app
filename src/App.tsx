c9569a17ab5c4a9f65ba983aea418d684ac89ef9
Chia Pak Mun <pakmunchia@ifca.com.my>
2020-06-05 17:22:34 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import * as React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import {
+  TopAppBar,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle,
+  TopAppBarFixedAdjust,
+} from '@rmwc/top-app-bar';
+import { useHistory } from 'react-router';
+import 'rmwc/dist/styles';
+import '@material/top-app-bar/dist/mdc.top-app-bar.css';
+import '@material/icon-button/dist/mdc.icon-button.css';
+import '@material/ripple/dist/mdc.ripple.css';
+import '@rmwc/icon/icon.css';
+import '@material/typography/dist/mdc.typography.css';
+import { Typography } from '@rmwc/typography';
 interface AppProps {}
 
-function App({}: AppProps) {
+function App(props: any) {
+  const history = useHistory();
+  console.log('here');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TopAppBar fixed>
+        <TopAppBarRow>
+          <TopAppBarSection>
+            <TopAppBarTitle>SnowPack React App</TopAppBarTitle>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <TopAppBarFixedAdjust />
+
+      <Typography use="body1">body1</Typography>
+      <Typography use="body2">body2</Typography>
+      <Typography use="caption">caption</Typography>
+      <Typography use="button">button</Typography>
+      <Typography use="overline">overline</Typography>
+    </>
   );
 }
 
